94135097e28badb2fcb11b8b85fb5de23154ba56
KomushKrya <94683228+KomushKrya@users.noreply.github.com>
2025-09-20 12:44:16 +0000

    Добавлены swap и revert_string

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,21 @@
 #include "revert_string.h"
+#include "string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+	if (str == NULL) return;
+    
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+    
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        
+        start++;
+        end--;
+    }
 }
 
